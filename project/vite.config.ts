6f6922487aacb9c036f79ff6f77ae1fa00135519
Enzaoooo <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>
2025-09-30 00:38:27 -0300

    gh pages

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/LovelySexDay-5/',
+  base: '/LovelySexDay-5/', // ajuste para o nome do seu repositório
+  build: {
+    outDir: 'docs', // pasta usada pelo GitHub Pages
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
